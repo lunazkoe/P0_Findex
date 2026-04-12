76bc068cd99bc57deb561647fdcc2c2a66b69312
lunazkoe <lunazkoe@gmail.com>
2026-04-12 23:10:52 +0900

    first commit

---
+++ P0_Findex_Backend/src/main/java/com/codeit/findex/P0FindexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0FindexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P0FindexBackendApplication.class, args);
+    }
+
+}
---
+++ P0_Findex_Backend/src/test/java/com/codeit/findex/P0FindexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.findex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P0FindexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
